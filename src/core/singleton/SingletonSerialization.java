6fc3d057dd839978417a87145abb397538677602
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2020-12-02 09:32:44 +0530

    Singleton + Serialization

---
+++ src/core/singleton/SingletonSerialization.java
@@ -0,0 +1,43 @@
+package core.singleton;
+
+import java.io.Serializable;
+
+public class SingletonSerialization implements Serializable {
+
+	/*
+	 * Step 1: Private constructor
+	 *
+	 * Step 2: Public Method which return Object of this class when null else same
+	 * object
+	 * 
+	 * Step 3: private static field of same class type
+	 * 
+	 */
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static SingletonSerialization instance;
+
+	private SingletonSerialization() {
+	}
+
+	public static SingletonSerialization getInstance() {
+		if (instance == null) {
+			/*
+			 * This is lazy initialization, as we are creating instance only when
+			 * getInstance() is called. to have this eagar initialization we can create its
+			 * instance while at the time of loading this class by keeping
+			 * 
+			 * private static SingletonExample instance = new SingletonExample();
+			 * 
+			 * Create instance only if instance is null else return as it is ie: Creating
+			 * only one instance
+			 */
+			instance = new SingletonSerialization();
+		}
+		return instance;
+	}
+
+}
--- src/core/singleton/test/Test.java
+++ src/core/singleton/test/Test.java
@@ -1,11 +1,18 @@
 package core.singleton.test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 import core.singleton.EagarSingletonExample;
 import core.singleton.SingletonExample;
+import core.singleton.SingletonSerialization;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		SingletonExample singletonExample1 = SingletonExample.getInstance();
 		SingletonExample singletonExample2 = SingletonExample.getInstance();
 
@@ -14,6 +21,21 @@ public class Test {
 		EagarSingletonExample eagarSingletonExample2 = EagarSingletonExample.getInstance();
 
 		System.out.println(eagarSingletonExample1 == eagarSingletonExample2);
+
+		SingletonSerialization singletonSerialization1 = SingletonSerialization.getInstance();
+		ObjectOutputStream objectOutputStream1 = new ObjectOutputStream(
+				new FileOutputStream(new File("D:\\Code\\core-patters\\src\\core\\singleton\\singleton.txt")));
+		objectOutputStream1.writeObject(singletonSerialization1);
+
+		SingletonSerialization singletonSerialization2 = SingletonSerialization.getInstance();
+		ObjectOutputStream objectOutputStream2 = new ObjectOutputStream(
+				new FileOutputStream(new File("D:\\Code\\core-patters\\src\\core\\singleton\\singleton.txt")));
+		objectOutputStream2.writeObject(singletonSerialization2);
+		objectOutputStream1.close();
+		objectOutputStream2.close();
+
+		System.out.println(objectOutputStream1 == objectOutputStream2);
+
 	}
 
 }
